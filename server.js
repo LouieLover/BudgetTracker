b2cc4e4799294594f72bb5adf88bf1d51e502fde
Larry X <papadocarock@gmail.com>
2020-10-26 16:13:03 -0400

    updating port

--- server.js
+++ server.js
@@ -21,9 +21,10 @@ mongoose.connect("mongodb://localhost/budget", {
     useFindAndModify: false
 });
 
+
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log("App running on port http://localhost:" + PORT);
 });
