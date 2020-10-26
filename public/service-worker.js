95f9a3a1904b753a7734bab1da174d4ecc9b44d8
Larry X <papadocarock@gmail.com>
2020-10-26 16:32:55 -0400

    working service worker.js file

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,9 +9,11 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "styles.css"
-];​
+];
+
 const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";​
+const DATA_CACHE_NAME = "data-cache-v1";
+
 // install
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
@@ -33,9 +35,11 @@ self.addEventListener("install", function(evt) {
         .catch((err) => {
             console.log(err);
         })
-    );​
+    );
+
     self.skipWaiting();
-});​
+});
+
 // activate
 self.addEventListener("activate", function(evt) {
     evt.waitUntil(
@@ -49,9 +53,11 @@ self.addEventListener("activate", function(evt) {
                 })
             );
         })
-    );​
+    );
+
     self.clients.claim();
-});​
+});
+
 // fetch
 self.addEventListener("fetch", function(evt) {
     if (evt.request.url.includes("/api/")) {
@@ -64,7 +70,8 @@ self.addEventListener("fetch", function(evt) {
                         // If the response was good, clone it and store it in the cache.
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
-                        }​
+                        }
+
                         return response;
                     })
                     .catch((err) => {
@@ -73,9 +80,11 @@ self.addEventListener("fetch", function(evt) {
                     });
             })
             .catch((err) => console.log(err))
-        );​
+        );
+
         return;
-    }​
+    }
+
     evt.respondWith(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.match(evt.request).then((response) => {
